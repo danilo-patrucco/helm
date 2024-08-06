8a2d90b10b2573060ba2c52defd2c047f9fc2ffc
Danilo Patrucco <danilo.patrucco@gmail.com>
2024-08-06 17:08:22 -0400

    add test chart and lintignore file+test
    Signed-off-by: Danilo Patrucco <danilo.patrucco@gmail.com>

--- pkg/lint/ignore_test.go
+++ pkg/lint/ignore_test.go
@@ -4,9 +4,94 @@ import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"helm.sh/helm/v3/pkg/lint/support"
+	"path/filepath"
 	"testing"
 	"text/template"
 )
+
+func TestNewIgnorer(t *testing.T) {
+	chartPath := "rules/testdata/withsubchartlintignore"
+	ignoreFilePath := filepath.Join(chartPath, ".helmlintignore")
+
+	ignorer := NewIgnorer(chartPath, ignoreFilePath, func(format string, args ...interface{}) {
+		t.Logf(format, args...) // Use testing's log function to output debug information
+	})
+
+	assert.NotNil(t, ignorer, "Ignorer should not be nil")
+
+	// Check if patterns were loaded
+	if len(ignorer.Patterns) == 0 {
+		t.Errorf("Expected patterns to be loaded from the file, but none were found")
+	}
+}
+
+func TestFilterErrors(t *testing.T) {
+	ignorer := &Ignorer{
+		ErrorPatterns: map[string][]string{
+			"error pattern": {"ignore this error"},
+		},
+	}
+
+	errors := []error{
+		fmt.Errorf("ignore this error"),
+		fmt.Errorf("keep this error"),
+	}
+
+	filteredErrors := ignorer.FilterErrors(errors)
+	assert.Len(t, filteredErrors, 1)
+	assert.EqualError(t, filteredErrors[0], "keep this error")
+}
+
+func TestFilterNoPathErrors(t *testing.T) {
+	ignorer := &Ignorer{
+		ErrorPatterns: map[string][]string{
+			"chart error": {"this should be ignored"},
+		},
+	}
+
+	messages := []support.Message{ /* your test messages */ }
+	errors := []error{fmt.Errorf("this should be ignored"), fmt.Errorf("this should be kept")}
+
+	filteredMessages, filteredErrors := ignorer.FilterNoPathErrors(messages, errors)
+	assert.Empty(t, filteredErrors)
+	assert.NotEmpty(t, filteredMessages)
+}
+
+func TestMatchNoPathError(t *testing.T) {
+	ignorer := &Ignorer{
+		ErrorPatterns: map[string][]string{
+			"generic error": {"ignore this"},
+		},
+	}
+
+	result := ignorer.MatchNoPathError("ignore this")
+	assert.False(t, result)
+}
+
+func TestDebug(t *testing.T) {
+	var captured string
+	debugFn := func(format string, args ...interface{}) {
+		captured = fmt.Sprintf(format, args...)
+	}
+
+	ignorer := &Ignorer{
+		debugFnOverride: debugFn,
+	}
+
+	ignorer.Debug("test %s", "debug")
+	assert.Equal(t, "test debug", captured)
+}
+
+func TestMatch(t *testing.T) {
+	ignorer := &Ignorer{
+		Patterns: map[string][]string{
+			"/path/to/file": {"error pattern"},
+		},
+	}
+
+	assert.True(t, ignorer.match("error pattern in /path/to/file"))
+	assert.False(t, ignorer.match("this should not match"))
+}
 
 func TestFilterIgnoredMessages(t *testing.T) {
 	type args struct {
@@ -25,7 +110,7 @@ func TestFilterIgnoredMessages(t *testing.T) {
 					{
 						Severity: 3,
 						Path:     "templates/",
-						Err:      template.ExecError{
+						Err: template.ExecError{
 							Name: "certmanager-issuer/templates/rbac-config.yaml",
 							Err:  fmt.Errorf(`template: certmanager-issuer/templates/rbac-config.yaml:1:67: executing "certmanager-issuer/templates/rbac-config.yaml" at <.Values.global.ingress>: nil pointer evaluating interface {}.ingress`),
 						},
@@ -58,7 +143,6 @@ func TestFilterIgnoredMessages(t *testing.T) {
 					Path:     "Chart.yaml",
 					Err:      fmt.Errorf("icon is recommended"),
 				},
-
 			},
 		},
 	}
