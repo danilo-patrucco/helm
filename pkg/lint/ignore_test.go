2f87aca5d69d27c4cc3c6b6a401df7cd45b20e4a
Daniel Pritchett ⚡ <dpritchett@gmail.com>
2024-08-09 08:43:03 -0500

    Revert "Spikes out a way to unit test lint ignore matches without reading fro…"
    This reverts commit 5f0f6f333576b9fcada583057861dabea3c42d4f.

--- pkg/lint/ignore_test.go
+++ pkg/lint/ignore_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"helm.sh/helm/v3/pkg/lint/support"
-	"strings"
 	"testing"
 	"text/template"
 )
@@ -26,7 +25,7 @@ func TestFilterIgnoredMessages(t *testing.T) {
 					{
 						Severity: 3,
 						Path:     "templates/",
-						Err: template.ExecError{
+						Err:      template.ExecError{
 							Name: "certmanager-issuer/templates/rbac-config.yaml",
 							Err:  fmt.Errorf(`template: certmanager-issuer/templates/rbac-config.yaml:1:67: executing "certmanager-issuer/templates/rbac-config.yaml" at <.Values.global.ingress>: nil pointer evaluating interface {}.ingress`),
 						},
@@ -59,6 +58,7 @@ func TestFilterIgnoredMessages(t *testing.T) {
 					Path:     "Chart.yaml",
 					Err:      fmt.Errorf("icon is recommended"),
 				},
+
 			},
 		},
 	}
@@ -67,44 +67,6 @@ func TestFilterIgnoredMessages(t *testing.T) {
 			ignorer := Ignorer{Patterns: tt.args.ignorePatterns}
 			got := ignorer.FilterMessages(tt.args.messages)
 			assert.Equalf(t, tt.want, got, "FilterMessages(%v, %v)", tt.args.messages, tt.args.ignorePatterns)
-		})
-	}
-}
-
-func TestIgnorer_PatternMatching(t *testing.T) {
-	tests := []struct {
-		name               string
-		ignoreFileContents string
-		givenMessages      []support.Message
-		givenErrors        []error
-		wantMessages       []support.Message
-		wantErrors         []error
-	}{
-		{
-			name:               "should suppress errors marked via " + errorPatternPrefix,
-			ignoreFileContents: "error_lint_ignore=chart metadata is missing these dependencies:*",
-			givenMessages: []support.Message{
-				{
-					Severity: 3,
-					Path:     "/fake/path/goes/here",
-					Err:      fmt.Errorf("chart metadata is missing these dependencies: gitaly,mailroom,migrations,sidekiq,webservice,toolbox,geo-logcursor,gitlab-exporter,gitlab-shell,kas,spamcheck"),
-				},
-			},
-			givenErrors: []error{
-				fmt.Errorf("chart metadata is missing these dependencies: gitaly,mailroom,migrations,sidekiq,webservice,toolbox,geo-logcursor,gitlab-exporter,gitlab-shell,kas,spamcheck"),
-			},
-			wantMessages: []support.Message{},
-			wantErrors:   []error{},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			ignorer := newIgnorerFromReader(strings.NewReader(tt.ignoreFileContents))
-			gotMessages, gotErrors := ignorer.FilterNoPathErrors(tt.givenMessages, tt.givenErrors)
-
-			assert.Equal(t, tt.wantMessages, gotMessages)
-			assert.Equal(t, tt.wantErrors, gotErrors)
 		})
 	}
 }
