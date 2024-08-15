d90fc3dc47b7e25e3c23fb32adad4bf51c8d0290
Daniel Pritchett ⚡ <dpritchett@radiusmethod.com>
2024-08-15 21:08:22 +0000

    Cleaning up our object model to make it easier to document with examples and tests (#26)
    * WIP PR cleanup - shrinking ignore rule loader
    
    * WIP PR cleanup - shrinking ignore rule loader
    
    * WIP PR cleanup - shrinking ignore rule loader
    
    * WIP PR cleanup - shrinking ignore rule loader
    
    * WIP PR cleanup - shrinking ignore rule loader

--- pkg/lint/ignore/ignorer.go
+++ pkg/lint/ignore/ignorer.go
@@ -1,89 +1,62 @@
 package ignore
 
 import (
+	"fmt"
 	"helm.sh/helm/v3/pkg/lint/support"
-	"log/slog"
-	"path/filepath"
-	"strings"
+	"log"
 )
 
+// DefaultIgnoreFileName is the name of the lint ignore file
+const DefaultIgnoreFileName = ".helmlintignore"
+
+var debugFn func(format string, v ...interface{})
+
 type Ignorer struct {
-	ChartPath  string
-	Rules      []Rule
-	logger     *slog.Logger
-	RuleLoader *RuleLoader
+	ChartPath string
+	Matchers  []MatchesErrors
 }
 
-type PathlessRule struct {
-	RuleText    string
-	MessageText string
-}
-
-// Ignorer is used to create the ignorer object that contains the ignore rules
-func NewActionIgnorer(chartPath string, lintIgnorePath string, debugLogFn func(string, ...interface{})) (*Ignorer, error) {
-	cmdIgnorer, err := NewRuleLoader(chartPath, lintIgnorePath, debugLogFn)
+func NewIgnorer(chartPath string, lintIgnorePath string, debugLogFn func(string, ...interface{})) (*Ignorer, error) {
+	debugFn = debugLogFn
+	matchers, err := LoadFromFilePath(chartPath, lintIgnorePath)
 	if err != nil {
 		return nil, err
 	}
 
-	return &Ignorer{ChartPath: chartPath, RuleLoader: cmdIgnorer}, nil
+	return &Ignorer{ChartPath: chartPath, Matchers: matchers}, nil
 }
 
-// FilterMessages Verify what messages can be kept in the output, using also the error as a verification (calling ShouldKeepError)
+// FilterMessages Verify what messages can be kept in the output, using also the error as a verification
 func (i *Ignorer) FilterMessages(messages []support.Message) []support.Message {
 	out := make([]support.Message, 0, len(messages))
 	for _, msg := range messages {
-		if i.ShouldKeepError(msg.Err) {
+		if i.ShouldKeepMessage(msg) {
 			out = append(out, msg)
 		}
 	}
 	return out
 }
 
+func (i *Ignorer) ShouldKeepMessage(msg support.Message) bool {
+	return i.ShouldKeepError(msg.Err)
+}
+
 // ShouldKeepError is used to verify if the error associated with the message need to be kept, or it can be ignored, called by FilterMessages and in the pkg/action/lint.go Run main function
 func (i *Ignorer) ShouldKeepError(err error) bool {
-	errText := err.Error()
-
 	// if any of our Matchers match the rule, we can discard it
-	for _, rule := range i.RuleLoader.Matchers {
-		match := rule.Match(errText)
-		if match != nil {
-			i.RuleLoader.Debug("lint ignore rule matched", match.LogAttrs())
+	for _, rule := range i.Matchers {
+		if rule.Match(err) {
+			debug("lint ignore rule matched this error, we should suppress it.", "errText", err.Error(), rule.LogAttrs())
 			return false
 		}
 	}
 
 	// if we can't find a reason to discard it, we keep it
+	debug("no lint ignore rules matched this error, we should NOT suppress it.", "errText", err.Error())
 	return true
 }
 
-type MatchesErrors interface {
-	Match(string) *RuleMatch
-}
-
-type RuleMatch struct {
-	ErrText  string
-	RuleText string
-}
-
-func (rm RuleMatch) LogAttrs() slog.Attr {
-	return slog.Group("rule_match", slog.String("err_text", rm.ErrText), slog.String("rule_text", rm.RuleText))
-}
-
-// Match errors that have no file path in their body with ignorer rules.
-// An examples of errors with no file path in their body is chart metadata errors `chart metadata is missing these dependencies`
-func (pr PathlessRule) Match(errText string) *RuleMatch {
-	ignorableError := pr.MessageText
-	parts := strings.SplitN(ignorableError, ":", 2)
-	prefix := strings.TrimSpace(parts[0])
-
-	if match, _ := filepath.Match(ignorableError, errText); match {
-		return &RuleMatch{ErrText: errText, RuleText: pr.RuleText}
-	}
-
-	if matched, _ := filepath.Match(prefix, errText); matched {
-		return &RuleMatch{ErrText: errText, RuleText: pr.RuleText}
-	}
-
-	return nil
+var defaultDebugFn = func(format string, v ...interface{}) {
+	format = fmt.Sprintf("[debug] %s\n", format)
+	_ = log.Output(2, fmt.Sprintf(format, v...))
 }
